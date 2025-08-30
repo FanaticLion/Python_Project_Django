f9d83a4ce6c5955265bb3f3766454116615fa603
fantasticlion <den20020805@gmail.com>
2025-08-30 14:11:21 +0300

    Завершено создание приложения blog: шаблоны, формы, URLs, админка

--- blog/forms.py
+++ blog/forms.py
@@ -6,3 +6,8 @@ class BlogPostForm(forms.ModelForm):
     class Meta:
         model = BlogPost
         fields = ['title', 'content', 'preview', 'is_published']
+        widgets = {
+            'title': forms.TextInput(attrs={'class': 'form-control'}),
+            'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 4}),
+            'is_published': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
+        }
